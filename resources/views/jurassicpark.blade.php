727474c8a5b0b16e3f5da5e1614763ee312d140d
David Oudenaarden <david.oudenaarden@outlook.com>
2023-03-06 21:07:19 +0100

    made terminals into links

--- resources/views/jurassicpark.blade.php
+++ resources/views/jurassicpark.blade.php
@@ -4,45 +4,53 @@
 
     <div class="text-white">
         <div class="flex flex-row justify-center ">
-            <x-jurassic-park.terminal>
+            <a href="/dinosaurs">
+                <x-jurassic-park.terminal>
 
-                <div class="m-1">
-                    <p>> Dinosaurs </br>
-                        > Status:
-                        <span class="text-green-500">
-                            online
-                        </span></br>
-                        > </br>
-                        > Expected Dinosaurs: {{ $parkDinosaursCount }} </br>
-                        > Counted Dinosaurs: 292 </br>
-                        > </br>
-                        > Last count: @php
-                            echo now(-1);
-                        @endphp
-                        </br>
-                        > </br>
-                        > Fences:
-                        <span class="text-green-500">
-                            online
-                        </span></br>
-                    </p>
-                </div>
+                    <div class="m-1">
+                        <p>> Dinosaurs </br>
+                            > Status:
+                            <span class="text-green-500">
+                                online
+                            </span></br>
+                            > </br>
+                            > Expected Dinosaurs: {{ $parkDinosaursCount }} </br>
+                            > Counted Dinosaurs: 292 </br>
+                            > </br>
+                            > Last count: @php
+                                echo now(-1);
+                            @endphp
+                            </br>
+                            > </br>
+                            > Fences:
+                            <span class="text-green-500">
+                                online
+                            </span></br>
+                        </p>
+                    </div>
 
-            </x-jurassic-park.terminal>
+                </x-jurassic-park.terminal>
+            </a>
 
-            <x-jurassic-park.terminal>
+            <a href="#">
+                <x-jurassic-park.terminal>
 
-                <div class="m-1"> test2 </div>
+                    <div class="m-1"> test2 </div>
 
-            </x-jurassic-park.terminal>
+                </x-jurassic-park.terminal>
+            </a>
 
-            <x-jurassic-park.terminal>
-                <div class="m-1"> test3 </div>
-            </x-jurassic-park.terminal>
+            <a href="#">
+                <x-jurassic-park.terminal>
+                    <div class="m-1"> test3 </div>
+                </x-jurassic-park.terminal>
+            </a>
 
-            <x-jurassic-park.terminal>
-                <div class="m-1"> test4 </div>
-            </x-jurassic-park.terminal>
+            <a href="#">
+                <x-jurassic-park.terminal>
+                    <div class="m-1"> test4 </div>
+                </x-jurassic-park.terminal>
+            </a>
         </div>
     </div>
 
